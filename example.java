7940f8a280d4b00b30b93c42711a7aeb10aa4250
ibrahim-kiri <ibrahimkiri@gmail.com>
2025-10-21 11:50:28 +0300

    Initial java practice

---
+++ example.java
@@ -0,0 +1,6 @@
+public class Example {
+    public static void main(String[] args) {
+        // Write the program here
+        System.out.println("Once upon a time \n there was \n a program");
+    }
+}
